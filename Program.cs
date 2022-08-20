7124a40e5574e54936777a78f4976527a547c3ff
darshshsh <ds14@gmail.com>
2022-08-20 13:43:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace swapnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            swap(a, b);
+            Console.WriteLine("Actual numbers: {0} and {1}", a, b);
+            gswap(ref a, ref b);
+            Console.WriteLine("Actual numbers: {0} and {1}", a, b);
+            Console.ReadLine();
+        }
+        static void swap(int a, int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("Swapped numbers: {0} and {1}", a, b);
+        }
+        static void gswap(ref int a, ref int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("Swapped numbers: {0} and {1}", a, b);
+
+
+        }
+    }
+}
