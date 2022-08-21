582a46c8ac4905a8a7738ee699ab4957fd9d72d0
darshshsh <ds14@gmail.com>
2022-08-21 13:09:05 +0530

    13th

--- Program.cs
+++ Program.cs
@@ -4,15 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Problem 11 - Integer Checking 2");
-            int a,b;
-            Console.WriteLine("Two numbers  // only if Both pos returns true");
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            if (a > 0 && b > 0)
-                Console.WriteLine("True");
-            else
-                Console.WriteLine("False");
+            Console.WriteLine(" Problem 13 - metre to kilometre conversion");
+            int m;
+            Console.WriteLine("Enter metres");
+            m = Convert.ToInt32(Console.ReadLine());
+            float km = m / 1000;
+            Console.WriteLine("meter to km is {0}", km);
+            int kilo;
+            Console.WriteLine("Enter kilometer");
+            kilo = Convert.ToInt32(Console.ReadLine());
+            int metre;
+            metre = 1000 * kilo;
+            Console.WriteLine("km to meter is {0}", metre);
             Console.ReadLine();
         }
     }
