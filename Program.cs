39aacd983a0bd83e988b77ce0bd53e910cab7bd0
darshshsh <ds14@gmail.com>
2022-08-21 12:16:33 +0530

    6th

--- Program.cs
+++ Program.cs
@@ -4,16 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Problem 5 ");
-            int a, b, c;
-            Console.WriteLine("Enter three numbers");
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            c = Convert.ToInt32(Console.ReadLine());
-            int op1 = (a + b) * c;
-            int op2 = (a * b) + (b * c);
-            Console.WriteLine("(x+y).z={0}", op1);
-            Console.WriteLine("(x.y)+(y.z)={0}", op2);
+            Console.WriteLine("Problem6 - Input Age");
+            int age;
+            Console.WriteLine("Enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You look older than {0}", age);
             Console.ReadLine();
         }
     }
