5378b3eddc86d23822cdb21b476a1338188f254e
darshshsh <ds14@gmail.com>
2022-08-21 12:06:59 +0530

    5th

--- Program.cs
+++ Program.cs
@@ -4,17 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("---Problem4---");
-            int a,b,c,d;
-            Console.WriteLine("Enter four numbers for average");
+            Console.WriteLine("Problem 5 ");
+            int a, b, c;
+            Console.WriteLine("Enter three numbers");
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
             c = Convert.ToInt32(Console.ReadLine());
-            d = Convert.ToInt32(Console.ReadLine());
-            int sum, avg;
-            sum = a+b+c+d;
-            avg = sum / 4;
-            Console.WriteLine("{0}/4={1}", sum, avg);
+            int op1 = (a + b) * c;
+            int op2 = (a * b) + (b * c);
+            Console.WriteLine("(x+y).z={0}", op1);
+            Console.WriteLine("(x.y)+(y.z)={0}", op2);
             Console.ReadLine();
         }
     }
