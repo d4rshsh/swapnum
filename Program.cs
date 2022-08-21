3df997fb441580f2095611f07904bace49d988f0
darshshsh <ds14@gmail.com>
2022-08-21 13:04:46 +0530

    11th

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Problem 10 - integer checking");
-            int a, b;
-            Console.WriteLine("Enter two integers");
+            Console.WriteLine(" Problem 11 - Integer Checking 2");
+            int a,b;
+            Console.WriteLine("Two numbers  // only if Both pos returns true");
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
-            if ((a < 0 && b> 0) || (a > 0 && b < 0))
+            if (a > 0 && b > 0)
                 Console.WriteLine("True");
             else
                 Console.WriteLine("False");
