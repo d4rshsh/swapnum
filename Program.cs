e67d0bbc5301542b51451a07d663a6a408956e07
darshshsh <ds14@gmail.com>
2022-08-21 11:57:54 +0530

    4th

--- Program.cs
+++ Program.cs
@@ -4,28 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--Problem--3");
-            Console.WriteLine("Arithmetic Operations - Add,Sub,Div,Mul,Rem");
-            int a, b;
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("---Problem4---");
+            int a,b,c,d;
+            Console.WriteLine("Enter four numbers for average");
             a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number");
             b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Addition");
-            int add = a + b;
-            Console.WriteLine("{0} + {1}={2}", a, b, add);
-            Console.WriteLine("Subtraction");
-            int sub = a - b;
-            Console.WriteLine("{0}-{1}={2}", a, b, sub);
-            Console.WriteLine("Multiplication");
-            int prod = a * b;
-            Console.WriteLine("{0}*{1}={2}", a, b, prod);
-            Console.WriteLine("division");
-            float div = a / b;
-            Console.WriteLine("{0}/{1}={2}", a, b, div);
-            Console.WriteLine("Remainder");
-            int rem = a % b;
-            Console.WriteLine("{0} mod {1}={2}", a, b, rem);
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+            int sum, avg;
+            sum = a+b+c+d;
+            avg = sum / 4;
+            Console.WriteLine("{0}/4={1}", sum, avg);
             Console.ReadLine();
         }
     }
