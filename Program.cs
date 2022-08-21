9f18010de74f93f1569f76476e0851d82022128d
darshshsh <ds14@gmail.com>
2022-08-21 12:44:09 +0530

    8th

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Problem 7 -- Pattern");
-            int n;
-            Console.WriteLine("Enter digit for pattern");
-            n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < 4; i++)
+            Console.WriteLine(" Problem8 - pattern rectangle");
+            int rect;
+            Console.WriteLine("Enter digit for rectangle pattern");
+            rect = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}{0}{0}", rect);
+            int re = 0;
+            while (re < 3)
             {
-                if (i % 2 == 0)
-                    Console.WriteLine("{0} {0} {0} {0}", n);
-                else
-                    Console.WriteLine("{0}{0}{0}{0}", n);
-
+                Console.WriteLine("{0} {0}", rect);
+                re++;
             }
+            Console.WriteLine("{0}{0}{0}", rect);
             Console.ReadLine();
         }
     }
