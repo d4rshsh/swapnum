91db6a9b093c90dfc296f6dd07e37fd5e3cd3457
darshshsh <ds14@gmail.com>
2022-08-23 19:27:30 +0530

    prob2/aug23

--- Program.cs
+++ Program.cs
@@ -1,36 +1,35 @@
-﻿namespace swapnum
+﻿namespace IncrementalAssignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a, b;
-            Console.WriteLine("Enter numbers");
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            swap(a, b);
-            Console.WriteLine("Actual numbers: {0} and {1}", a, b);
-            gswap(ref a, ref b);
-            Console.WriteLine("Actual numbers: {0} and {1}", a, b);
+            string first_name, last_name;
+            Console.WriteLine("Enter your first name");
+            first_name = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            last_name = Console.ReadLine();
+            string user_name, pwd;
+            Console.WriteLine("Enter uname");
+            user_name = Console.ReadLine();
+            Console.WriteLine("Password");
+            pwd = Console.ReadLine();
+            if ((first_name == user_name) && (last_name == pwd) && (!String.IsNullOrEmpty(user_name)) && (!String.IsNullOrEmpty(pwd)))
+                Console.WriteLine("Welcome to my app" + " " + first_name + " " + last_name);
+            else
+            {
+
+                if (first_name != user_name && last_name != pwd)
+                    Console.WriteLine("Invalid Credentials");
+                else if (first_name != user_name)
+                    Console.WriteLine("uname unmatched");
+                else if ((String.IsNullOrEmpty(user_name)) || (String.IsNullOrEmpty(pwd)))
+                    Console.WriteLine("Username or password can't be null");
+                else
+                    Console.WriteLine("Invalid Password");
+
+            }
             Console.ReadLine();
-        }
-        static void swap(int a, int b)
-        {
-            int c;
-            c = a;
-            a = b;
-            b = c;
-            Console.WriteLine("Swapped numbers: {0} and {1}", a, b);
-        }
-        static void gswap(ref int a, ref int b)
-        {
-            int c;
-            c = a;
-            a = b;
-            b = c;
-            Console.WriteLine("Swapped numbers: {0} and {1}", a, b);
-
-
         }
     }
 }
